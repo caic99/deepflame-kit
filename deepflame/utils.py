73f6a397780a897572c93b721fc4b40b88690ec9
caic99 <amoycaic@gmail.com>
2024-01-31 19:32:10 +0800

    Add epsilon to std in preventing div 0

--- deepflame/utils.py
+++ deepflame/utils.py
@@ -1,5 +1,6 @@
 def boxcox(
-    x, lmbda: float,
+    x,
+    lmbda: float,
 ):  # https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.boxcox.html
     return (x**lmbda - 1) / lmbda  # if lmbda != 0 else log(x)
 
@@ -8,10 +9,13 @@ def inv_boxcox(y, lmbda: float):
     return (y * lmbda + 1) ** (1 / lmbda)  # if lmbda != 0 else exp(x)
 
 
+epsilon = 1e-8
+
+
 def normalize(x, mean, std):
     # TODO:handle the case for std = 0
-    return (x - mean) / std
+    return (x - mean) / (std + epsilon)
 
 
 def denormalize(y, mean, std):
-    return y * std + mean
+    return y * (std + epsilon) + mean
