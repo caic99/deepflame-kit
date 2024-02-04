fix duplicated normalization for T and P

deepflame/trainer.py
@@ -40,8 +40,6 @@ class Trainer(L.LightningModule):
             Y_label,
             H_label,
         ) = batch
-        T_norm_in = normalize(T_in, self.model.T_in_mean, self.model.T_in_std)
-        P_norm_in = normalize(P_in, self.model.P_in_mean, self.model.P_in_std)
         Y_t_in = boxcox(Y_in, self.model.lmbda)
         Y_t_label = boxcox(Y_label, self.model.lmbda)
         Y_dt_label = Y_t_label - Y_t_in
@@ -50,7 +48,7 @@ class Trainer(L.LightningModule):
             self.model.Y_dt_mean,
             self.model.Y_dt_std,
         )
-        Y_pred, Y_dt_pred = self.forward(T_norm_in, P_norm_in, Y_t_in)
+        Y_pred, Y_dt_pred = self.forward(T_in, P_in, Y_t_in)
 
         criterion = nn.L1Loss()
         loss1 = criterion(Y_dt_pred, Y_dt_label)